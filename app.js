0e960177e322bcba2050ece590117b853882e23c
Olawale06 <bamideleolawale13@gmail.com>
2023-04-16 23:55:17 +0100

    Commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Item = require('./models/items');
+const Item = require('./items');
 const mongoose = require('mongoose');
 const app = express();
 
--- items.js
+++ items.js
@@ -1,20 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const itemSchema = new Schema({
-    name:{
-        type:String,
-        require:true
+const itemSchema = new Schema(
+  {
+    name: {
+      type: String,
+      require: true,
     },
-    price:{
-        type:Number,
-        require:true
-    }
-},{timestamps:true});
+    price: {
+      type: Number,
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
 
-const Item = mongoose.model('Item', itemSchema)
+const Item = mongoose.model("Item", itemSchema);
 module.exports = Item;
-
 
 // const mongoose = require('mongoose');
 // const userSchema = new mongoose.Schema({
